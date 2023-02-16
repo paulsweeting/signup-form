3e22e44132e959e03604b9e8950757ab4760f9d4
Mustafa <travellingtypist1@gmail.com>
2023-02-16 17:45:10 +0000

    Initial commit

---
+++ src/components/SignUp/Details/Details.tsx
@@ -0,0 +1,71 @@
+import { Field, Form, useFormikContext } from "formik";
+
+const Details = () => {
+  const { values, errors, touched } = useFormikContext<{
+    email: string;
+    firstName: string;
+    lastName: string;
+    password: string;
+  }>();
+  return (
+    <Form>
+      <div className="w-[600px] mx-auto flex flex-col space-y-10 mt-[7%] box border border-black p-14 rounded-lg bg-gray-400">
+        <label className="flex flex-col mb-1 font-medium text-md">
+          First Name{" "}
+          <Field
+            name="firstName"
+            className={`rounded h-8 outline-none p-3 ${
+              touched.firstName && errors.firstName && " border border-red-600"
+            }`}
+            input
+          />
+          <p className="text-red-600 text-sm">
+            {touched.firstName && errors.firstName}
+          </p>
+        </label>
+        <label className="flex flex-col mb-1 font-medium text-md">
+          Last Name
+          <Field
+            name="lastName"
+            className={`border border-black rounded h-8 outline-none p-3 ${
+              touched.lastName && errors.lastName && "border-red-600"
+            }`}
+            input
+          />
+          <p className="text-red-600 text-sm">
+            {touched.lastName && errors.lastName}
+          </p>
+        </label>
+
+        <label className="flex flex-col mb-1 font-medium text-md">
+          Email
+          <Field
+            name="email"
+            className={`border border-black rounded h-8 outline-none p-3 ${
+              touched.email && errors.email && "border-red-600"
+            }`}
+            input
+          />
+          <p className="text-red-600 text-sm">
+            {touched.email && errors.email}
+          </p>
+        </label>
+        <label className="flex flex-col mb-1 font-medium text-md">
+          Password
+          <Field
+            name="password"
+            className={`border border-black rounded h-8 outline-none p-3 ${
+              touched.password && errors.password && "border-red-600"
+            }`}
+            input
+          />
+          <p className="text-red-600 text-sm">
+            {touched.password && errors.password}
+          </p>
+        </label>
+        <button className="bg-purple-600 w-24 h-12 rounded">Submit</button>
+      </div>
+    </Form>
+  );
+};
+export default Details;
---
+++ src/components/SignUp/SignUp.tsx
@@ -0,0 +1,47 @@
+import { Field, Form, Formik } from "formik";
+import { useState } from "react";
+import * as Yup from "yup";
+import Details from "./Details/Details";
+
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  age: "",
+  email: "",
+  password: "",
+};
+
+const validation = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  lastName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  password: Yup.string()
+    .min(8, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+});
+
+const SignUp = () => {
+  const [submitted, setSubmitted] = useState(false);
+
+  return (
+    <div>
+      {submitted && <h1>Submitted Successfully</h1>}
+      <Formik
+        initialValues={initialValues}
+        onSubmit={() => setSubmitted(true)}
+        validationSchema={validation}
+      >
+        <Details />
+      </Formik>
+    </div>
+  );
+};
+
+export default SignUp;
