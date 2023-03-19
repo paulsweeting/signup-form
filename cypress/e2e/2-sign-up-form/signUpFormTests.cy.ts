6973c8bd8e36e7d43549176654305ba5e949e8ca
Paul <paul@Shauns-MacBook-Pro.local>
2023-03-19 11:52:00 +0000

    Negative Tests - Field too short, field too long, invalid password

--- cypress/e2e/2-sign-up-form/signUpFormTests.cy.ts
+++ cypress/e2e/2-sign-up-form/signUpFormTests.cy.ts
@@ -12,7 +12,7 @@ describe('Signup', () => {
 });
 
 // Negative Tests
-describe('Single missing required field', () => {
+describe('Single required field missing', () => {
   it('should inform the user the field is required', () => {
     SignupForm.visit();
 
@@ -31,7 +31,7 @@ describe('Single missing required field', () => {
     // Assert the user is told that the first name field is required
     cy.get(SignupForm.firstNameErrorMessage).should('contain', 'Required');
 
-    // Check that the error is only displayed once within the form.
+    // Assert that the error is only displayed once within the form.
     cy.get('[data-cy=sign-up-form]')
       .find('[data-cy$=-error-message]') //Find all error messages
       .filter(':contains("Required")') // Only return those displaying the 'Required' error
@@ -51,12 +51,93 @@ describe('All required fields missing', () => {
     cy.get(SignupForm.signUpSuccessMessage)
       .should('not.contain', 'Submitted Successfully');
 
-    // Check that the error is displayed for the number of required fields
+    // Assert that the error is displayed for the number of required fields
     cy.get('[data-cy=sign-up-form]')
       .find('[data-cy$=-error-message]') // select all error messages
       .should('have.length', 4) // assert that there are 4 matching elements
       .each(($el) => {
         expect($el).to.contain('Required'); // assert that each element contains the text 'Required'
       });
+    });
+});
+
+describe('Entered text is too short', () => {
+  it('should inform the user that text they entered into the field is too short', () => {
+    SignupForm.visit();
+
+    // Short Second name
+    SignupForm.submitForm('Joe', 'D', 'test@example.com', 'password');
+
+    // Attempt to submit form
+    SignupForm.clickSubmitButton();
+
+    // Assert that the user is NOT signed up
+    cy.get(SignupForm.signUpSuccessMessage)
+      .should('not.contain', 'Submitted Successfully');
+
+    // Assert the user is told that the second name field is too short
+    cy.get(SignupForm.lastNameErrorMessage).should('contain', 'Too Short!');
+
+    // Assert that the error is only displayed once within the form.
+    cy.get('[data-cy=sign-up-form]')
+      .find('[data-cy$=-error-message]') //Find all error messages
+      .filter(':contains("Too Short!")') // Only return those displaying the 'Too Short!' error
+      .its('length')
+      .should('eq', 1); // Assert that only is error is displayed
+    });
+});
+
+describe('Entered text is too long', () => {
+  it('should inform the user that text they entered into the field is too long', () => {
+    SignupForm.visit();
+
+    // Long password
+    SignupForm.submitForm('Joe',
+                            'Doe',
+                            'test@example.com',
+                            'thispasswordisreallyreallyreallyreallyreallyreallyreallylong');
+
+    // Attempt to submit form
+    SignupForm.clickSubmitButton();
+
+    // Assert that the user is NOT signed up
+    cy.get(SignupForm.signUpSuccessMessage)
+      .should('not.contain', 'Submitted Successfully');
+
+    // Assert the user is told that the password entered is too long
+    cy.get(SignupForm.passwordErrorMessage).should('contain', 'Too Long!');
+
+    // Assert that the error is only displayed once within the form.
+    cy.get('[data-cy=sign-up-form]')
+      .find('[data-cy$=-error-message]') //Find all error messages
+      .filter(':contains("Too Long!")') // Only return those displaying the 'Too Long!' error
+      .its('length')
+      .should('eq', 1); // Assert that only is error is displayed
+    });
+});
+
+describe('Invalid email', () => {
+  it('should inform the user that the email they entered is invalid', () => {
+    SignupForm.visit();
+
+    // Invalid Email
+    SignupForm.submitForm('Joe','Doe','invalidemail','password');
+
+    // Attempt to submit form
+    SignupForm.clickSubmitButton();
+
+    // Assert that the user is NOT signed up
+    cy.get(SignupForm.signUpSuccessMessage)
+      .should('not.contain', 'Submitted Successfully');
+
+    // Assert the user is told that email is invalid
+    cy.get(SignupForm.emailErrorMessage).should('contain', 'Invalid email');
+
+    // Assert that the error is only displayed once within the form.
+    cy.get('[data-cy=sign-up-form]')
+      .find('[data-cy$=-error-message]') //Find all error messages
+      .filter(':contains("Invalid email")') // Only return those displaying the 'Invalid email' error
+      .its('length')
+      .should('eq', 1); // Assert that only is error is displayed
     });
 });
